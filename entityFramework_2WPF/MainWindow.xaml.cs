d9c008cf54c8e0f698a2cdcc434b561c7015e867
WebSpruce <kacperswi12363@gmail.com>
2023-09-16 13:10:09 +0200

    MVVM first command and general UI.
    Added popup for add value and adjusted methods for this.

--- entityFramework_2WPF/MainWindow.xaml.cs
+++ entityFramework_2WPF/MainWindow.xaml.cs
@@ -1,13 +1,18 @@
 ﻿using entityFramework_2WPF.ViewModels;
 using System.Windows;
+using System.Windows.Controls.Primitives;
 
 namespace entityFramework_2WPF
 {
     public partial class MainWindow : Window
     {
+        public Popup PopupPopup;
+        public static MainWindow? instance;
         public MainWindow()
         {
             InitializeComponent();
+            instance = this;
+            PopupPopup = AddValuePopup;
             DataContext = new MainViewModel();
         }
     }
--- entityFramework_2WPF/ViewModels/MainViewModel.cs
+++ entityFramework_2WPF/ViewModels/MainViewModel.cs
@@ -1,10 +1,35 @@
 ﻿
+using CommunityToolkit.Mvvm.Input;
 using entityFramework_2WPF.Data;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace entityFramework_2WPF.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        private bool addValueIsChecked;
+        public bool AddValueIsChecked
+        {
+            get { return addValueIsChecked; }
+            set
+            {
+                addValueIsChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand OpenFileCommand { get; private set; }
+        public ICommand ExportCommand { get; private set; }
+        public ICommand AddValueCommand { get; private set; }
+        public ICommand ResetDataCommand { get; private set; }
+        public ICommand ClosePopupCommand { get; private set; }
+        public ICommand AddValueBtnCommand { get; private set; }
+        public ICommand CancelBtnCommand { get; private set; }
+
         private ShopContext shopContext;
 
         public static MainViewModel? instance;
@@ -14,6 +39,13 @@ namespace entityFramework_2WPF.ViewModels
 
             shopContext = new ShopContext();
             CheckDatabaseConnection();
+
+            OpenFileCommand = new RelayCommand(() => OpenFile());
+            ExportCommand = new RelayCommand(() => ExportFile());
+            AddValueCommand = new RelayCommand(() => { AddValueIsChecked = true; });
+            ResetDataCommand = new RelayCommand(() => ResetData());
+            AddValueBtnCommand = new RelayCommand(() => AddValue());
+            CancelBtnCommand = new RelayCommand(() => { AddValueIsChecked = !MainWindow.instance.PopupPopup.IsOpen; });
         }
 
         private async void CheckDatabaseConnection()
@@ -22,6 +54,28 @@ namespace entityFramework_2WPF.ViewModels
             {
                 await shopContext.Database.EnsureCreatedAsync();
             }
+        }
+        private void OpenFile()
+        {
+            
+        }
+        private void ExportFile()
+        {
+
+        }
+        private void AddValue()
+        {
+            
+        }
+        private void ResetData()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
